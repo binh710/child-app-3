d7c09fbddb9e74e4771fedc24c69a8854830a6da
Binh <thanhbinh710@gmail.com>
2021-06-06 17:21:52 +0800

    add router, sync route change to stitcher app

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="nav">
+    <router-link @click="updateParentRoute('/')" to="/">Home</router-link> |
+    <router-link @click="updateParentRoute('/about')" to="/about">About</router-link>
+  </div>
+  <router-view/>
+  <p>
+    This application aims to show you how route syncing can be done across the parent & child app
+  </p>
+  <h3>
+    Current child route is: {{route}}
+  </h3>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      stitcherAppDomain: process.env.VUE_APP_STITCHER_DOMAIN
+    }
+  },
+  computed: {
+    route() {
+      return this.$route.fullPath
+    }
+  },
+  methods: {
+    updateParentRoute(url) {
+      if (url !== this.route) {
+        const msg = {
+          action: 'update route',
+          route: url
+        }
+        try {
+          window.parent.postMessage(msg, this.stitcherAppDomain);
+        } catch(e) {
+          console.error('error posting message to stitcher app', e);
+        }
+      }
+    }
+  },
 }
 </script>
 
@@ -21,6 +50,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="home">
+    <h1>Welcome to Your Child app</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+}
+</script>
